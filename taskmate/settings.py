ad5301a249676423f149400783b6a8951cd3f02c
subodhchalak <subodhchalak007@gmail.com>
2021-12-01 20:22:42 +0530

    Updated settings, requirements and new Procfile

--- taskmate/settings.py
+++ taskmate/settings.py
@@ -140,6 +140,8 @@ STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
+STATIC_ROOT = [os.path.join(BASE_DIR, 'staticfiles')]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
@@ -154,3 +156,4 @@ LOGIN_REDIRECT_URL = 'todolist'           # after successful login, user will be
 
 
 django_heroku.settings(locals())
+
